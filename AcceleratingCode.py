6f235778cde70bef5b1272e1662373a830fd1b4c
イゴーリ <f55dota217@gmail.com>
2021-12-05 18:10:07 +0300

    With jit acceleration

--- AcceleratingCode.py
+++ AcceleratingCode.py
@@ -1,6 +1,17 @@
 from numba import cuda, jit, njit, vectorize
 from matplotlib import pyplot as plt
+from pylab import imshow, show
+import numpy as np
+from timeit import default_timer as timer
+import sys
 
+def progress_bar(current, total, barLength=50):
+    progress = float(current) * 100 / total
+    arrow = '#' * int(progress / 100 * barLength - 1)
+    spaces = ' ' * (barLength - len(arrow))
+    sys.stdout.write('\rСоздаётся фрактал: [%s%s]' % (arrow, spaces))
+
+#@jit
 def mandel(x, y, max_iters):
 
     c = complex(x, y)
@@ -11,3 +22,27 @@ def mandel(x, y, max_iters):
             return i
     
     return max_iters
+#@jit
+def create_fractal(min_x, max_x, min_y, max_y, image, iters):
+    height = image.shape[0]
+    width = image.shape[1]
+
+    pixel_size_x = (max_x - min_x) / width
+    pixel_size_y = (max_y - min_y) / height
+
+    for x in range(width):
+        real = min_x + x * pixel_size_x
+        for y in range(height):
+            img = min_y + y * pixel_size_y
+            color = mandel(real, img, iters)
+            image[y, x] = color
+        progress_bar(x, width, 75)
+user_iters = int(input("Введите количество итераций фрактала: "))
+image = np.zeros((1024, 1536), dtype = np.uint8)
+start = timer()
+create_fractal(-2.0, 1.0, -1.0, 1.0, image, user_iters)
+seconds = timer() - start
+
+print('\nСоздан фрактал за %f секунд' % seconds)
+imshow(image)
+show()
